e3c10ddc582bab70f91b5c69d2fabb3204a48c96
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-05-19 21:28:36 +0700

    Minor fix

--- FE_ReactTS/src/main.tsx
+++ FE_ReactTS/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
-import Index from './routes/index.tsx';
 import './App.css'
+import Index from './routes/Index';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
