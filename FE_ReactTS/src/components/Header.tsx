d9ddb40b99d177960914fc4453fe79b253d7f664
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-05-23 18:49:30 +0700

    update folder structure.

--- FE_ReactTS/src/components/Header.tsx
+++ FE_ReactTS/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { Link } from "react-router";
-import { QADropdown } from "./layouts/home/QADropdown";
+import { QADropdown } from "./home/QADropdown";
 
 const Header = () => {
     return (
--- FE_ReactTS/src/pages/Public/Home.tsx
+++ FE_ReactTS/src/pages/Public/Home.tsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import Timetoast from '../../components/Timetoast'
-import SearchBar from '@/components/layouts/home/searchBar'
+import SearchBar from '@/components/home/searchBar'
 import { useState } from 'react'
 import CustomCircularLoading from '@/components/layouts/CustomCircularLoading'
 
