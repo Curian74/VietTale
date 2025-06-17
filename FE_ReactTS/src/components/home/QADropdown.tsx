0bc34f263ef3699a4ec026c8e4a20f230430e360
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-06-17 21:47:59 +0700

    Fix bug.

--- FE_ReactTS/src/components/home/QADropdown.tsx
+++ FE_ReactTS/src/components/home/QADropdown.tsx
@@ -19,7 +19,7 @@ export function QADropdown() {
           </svg>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 mt-3">
+      <DropdownMenuContent className="w-56 z-100">
         <DropdownMenuGroup className="font-medium">
           <DropdownMenuItem className="cursor-pointer">
             Lịch sử 12
