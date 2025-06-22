00a3a1180d98241f1b491af6619d1e67718f50e1
Cuong Pham Quoc <cuongpq114@gmail.com>
2025-06-22 21:23:18 +0700

    a

--- FE_ReactTS/src/main.tsx
+++ FE_ReactTS/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router";
 import './App.css'
 import Index from './routes/Index';
 import { AuthProvider } from './contexts/AuthProvider';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <Index />
+        <Analytics/>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
